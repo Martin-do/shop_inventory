185c2ca8f19ae04c0b45c00a53dbd92293664907
Martin-do <martinjaiyeola40@gmail.com>
2026-07-24 22:23:40 +0100

    Add /login/ and /logout/ shortcut redirects

--- shop_inventory/urls.py
+++ shop_inventory/urls.py
@@ -4,9 +4,13 @@ from django.contrib import admin
 from django.urls import include, path
 
 
+from django.views.generic import RedirectView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("login/", RedirectView.as_view(url="/accounts/login/", permanent=False)),
+    path("logout/", RedirectView.as_view(url="/accounts/logout/", permanent=False)),
     path("", include("inventory.urls")),
 ]
 
