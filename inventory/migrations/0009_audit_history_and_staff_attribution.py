c0e085dd4faf30b9920557381c5e65da9adf95b9
Martin <53823934+Martin-do@users.noreply.github.com>
2026-08-04 13:24:16 +0100

    Link audit migration to latest inventory schema

--- inventory/migrations/0009_audit_history_and_staff_attribution.py
+++ inventory/migrations/0009_audit_history_and_staff_attribution.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("inventory", "0001_initial"),
+        ("inventory", "0006_userprofile"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
